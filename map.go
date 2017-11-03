66ea3931f71d39ce47741841b69680c403be3113
Ulrich Kautz <ulrich.kautz@gmail.com>
2017-11-03 08:57:33 +0100

    Implemented Map casting

---
+++ map.go
@@ -0,0 +1,123 @@
+package cast
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// CastMap accepts any map and casts its into the most generic map[interface{}]interface{} - second
+// return parameter is false, if any key or value cannot be casted back to interface (which should not happen)
+func CastMap(from interface{}) (map[interface{}]interface{}, bool) {
+	t := createInterfaceType()
+	if val, err := CastMapValue(vof(from), t, t); err == nil {
+		return val.Interface().(map[interface{}]interface{}), true
+	}
+	return nil, false
+}
+
+// CastMapString accepts any map and casts its into map[string]interface{} - second return parameter
+// is false, if any key cannot be casted to string
+func CastMapString(from interface{}) (map[string]interface{}, bool) {
+	kt := createStringType()
+	vt := createInterfaceType()
+	if val, err := CastMapValue(vof(from), kt, vt); err == nil {
+		return val.Interface().(map[string]interface{}), true
+	}
+	return nil, false
+}
+
+// CastMapStringString accepts any map and casts its into map[string]string - second return parameter
+// is false, if any key or value cannot be casted to string
+func CastMapStringString(from interface{}) (map[string]string, bool) {
+	t := createStringType()
+	if val, err := CastMapValue(vof(from), t, t); err == nil {
+		return val.Interface().(map[string]string), true
+	}
+	return nil, false
+}
+
+// CastMapStringInt accepts any map and casts its into map[string]int64 - second return parameter
+// is false, if any key cannot be casted to string or any value cannot be casted into int64
+func CastMapStringInt(from interface{}) (map[string]int64, bool) {
+	kt := createStringType()
+	vt := createIntType()
+	if val, err := CastMapValue(vof(from), kt, vt); err == nil {
+		return val.Interface().(map[string]int64), true
+	}
+	return nil, false
+}
+
+// CastMapStringInt accepts any map and casts its into map[string]float64 - second return parameter
+// is false, if any key cannot be casted to string or any value cannot be casted into float64
+func CastMapStringFloat(from interface{}) (map[string]float64, bool) {
+	kt := createStringType()
+	vt := createFloatType()
+	if val, err := CastMapValue(vof(from), kt, vt); err == nil {
+		return val.Interface().(map[string]float64), true
+	}
+	return nil, false
+}
+
+// CastMapValue accepts any map and casts its into map of provided key and value types - second
+// return parameter contains error, if casting to given types is not possible
+func CastMapValue(src reflect.Value, keyType, valType reflect.Type) (*reflect.Value, error) {
+	return CastPartialMapValue(src, keyType, valType, true)
+}
+
+// CastPartialMapValue accepts any map and casts its into map of provided key and value types. The 4th
+// (optional) input parameter controls whether a partial map of all the key/values which could be casted
+// as requested (castFail = false or leave it out) is returned or whether only a map of provided types
+// is returned if ALL keys and values can be casted (castFail = true)
+func CastPartialMapValue(src reflect.Value, keyType, valType reflect.Type, castFail ...bool) (*reflect.Value, error) {
+	failOnFail := len(castFail) > 0 && castFail[0]
+	src = reflect.Indirect(src)
+	if src.Kind() != reflect.Map {
+		return nil, fmt.Errorf("can only cast from map kind to map kind, but got %s kind", src.Kind())
+	}
+	res := reflect.MakeMap(reflect.MapOf(keyType, valType))
+	keyKind := keyType.Kind()
+	valKind := valType.Kind()
+	for _, srcKey := range src.MapKeys() {
+		var resKey, resValue reflect.Value
+		if keyKind == reflect.Interface {
+			resKey = srcKey
+		} else if resKeyP := CastToValue(srcKey, keyKind); resKeyP != nil {
+			resKey = *resKeyP
+		} else if failOnFail {
+			return nil, fmt.Errorf("cannot cast key of kind %s to required kind %s", srcKey.Kind(), keyKind)
+		} else {
+			continue
+		}
+		if res.MapIndex(resKey).IsValid() {
+			return nil, fmt.Errorf("cannot use duplicate key %###v", resKey)
+		}
+		srcVal := src.MapIndex(srcKey)
+		if valKind == reflect.Interface {
+			resValue = srcVal
+		} else if resValP := CastToValue(srcVal, valKind); resValP != nil {
+			resValue = *resValP
+		} else if failOnFail {
+			return nil, fmt.Errorf("cannot cast value of kind %s to required kind %s", srcVal.Kind(), valKind)
+		} else {
+			continue
+		}
+		res.SetMapIndex(resKey, resValue)
+	}
+	if !failOnFail && res.Len() == 0 && src.Len() > 0 {
+		return nil, fmt.Errorf("could not cast any keys or values into requested %s key and % value type", keyType, valType)
+	}
+	return &res, nil
+}
+
+func createStringType() reflect.Type {
+	return reflect.TypeOf((*string)(nil)).Elem()
+}
+func createIntType() reflect.Type {
+	return reflect.TypeOf((*int64)(nil)).Elem()
+}
+func createFloatType() reflect.Type {
+	return reflect.TypeOf((*float64)(nil)).Elem()
+}
+func createInterfaceType() reflect.Type {
+	return reflect.TypeOf((*interface{})(nil)).Elem()
+}
---
+++ map_test.go
@@ -0,0 +1,124 @@
+package cast
+
+import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
+	"reflect"
+	"testing"
+)
+
+type _testBadType func() bool
+type _testStringo struct {
+	s string
+}
+
+var _testBadVar = _testBadType(func() bool { return false })
+
+func (s *_testStringo) String() string {
+	return fmt.Sprintf("stringo:%s", s.s)
+}
+
+func _testCastMap() map[interface{}]interface{} {
+
+	return map[interface{}]interface{}{
+		"a":              "b",
+		"c":              1,
+		int32(2):         3,
+		uint(4):          "5",
+		float64(6.5):     float64(1.5),
+		interface{}(nil): &_testBadVar,
+		uint8(9):         interface{}(nil),
+	}
+}
+
+func TestCastPartialMapValue(t *testing.T) {
+	res, err := CastPartialMapValue(vof(_testCastMap()), createInterfaceType(), createInterfaceType())
+	assert.Nil(t, err, "Could be casted")
+	assert.Equal(t, _testCastMap(), res.Interface(), "Result matches input")
+
+	resm, ok := CastMap(res.Interface())
+	assert.True(t, ok, "Valid map could be casted")
+	assert.Equal(t, _testCastMap(), resm, "Result matches input")
+
+	resm, ok = CastMap("str")
+	assert.False(t, ok, "Invalid source should not be casted")
+	resm, ok = CastMap(999)
+	assert.False(t, ok, "Invalid source should not be casted")
+
+	res, err = CastPartialMapValue(vof(_testCastMap()), createStringType(), createInterfaceType())
+	assert.Nil(t, err, "Could be casted")
+	assert.Equal(t, map[string]interface{}{"c": 1, "2": 3, "4": "5", "6.5": 1.5, "a": "b", "9": interface{}(nil)}, res.Interface(), "Result matches input")
+
+	res, err = CastPartialMapValue(vof(_testCastMap()), createIntType(), createFloatType())
+	assert.Nil(t, err, "Could be casted")
+	assert.Equal(t, map[int64]float64{2: 3, 4: 5, 6: 1.5}, res.Interface(), "Result matches input")
+
+	m := map[interface{}]interface{}{
+		"stringo:foo":        "valid key",
+		&_testStringo{"foo"}: "same string representation as above string",
+	}
+	assert.Equal(t, 2, len(m), "Yes, these arw two keys")
+	res, err = CastPartialMapValue(vof(m), createStringType(), createInterfaceType())
+	assert.NotNil(t, err, "Could be casted")
+	assert.Equal(t, "cannot use duplicate key \"stringo:foo\"", err.Error(), "stringo, the foo thief")
+
+	res, err = CastPartialMapValue(vof("str"), createIntType(), createFloatType())
+	assert.NotNil(t, err, "Could be casted")
+	assert.Equal(t, "can only cast from map kind to map kind, but got string kind", err.Error(), "Result matches input")
+	res, err = CastPartialMapValue(vof(123), createIntType(), createFloatType())
+	assert.NotNil(t, err, "Could be casted")
+	assert.Equal(t, "can only cast from map kind to map kind, but got int kind", err.Error(), "Result matches input")
+
+}
+
+func TestCastMapString(t *testing.T) {
+	res, err := CastPartialMapValue(vof(_testCastMap()), createStringType(), createInterfaceType())
+	assert.Nil(t, err, "No casting error")
+	assert.Equal(t, map[string]interface{}{"4": "5", "6.5": 1.5, "a": "b", "c": 1, "2": 3, "9": interface{}(nil)}, res.Interface(), "Casted matches assumption")
+	resm, ok := CastMapString(res.Interface())
+	assert.True(t, ok, "Valid map could be casted")
+	assert.Equal(t, map[string]interface{}{"4": "5", "6.5": 1.5, "a": "b", "c": 1, "2": 3, "9": interface{}(nil)}, resm, "Result matches input")
+
+	res, err = CastPartialMapValue(vof(_testCastMap()), createStringType(), reflect.TypeOf((*_testBadType)(nil)).Elem())
+	assert.NotNil(t, err, "Should fail to cast with unsupported value type")
+	assert.Contains(t, err.Error(), "could not cast any keys or values into requested", "Failed cause nothing was casted")
+
+	m := _testCastMap()
+	v := _testBadType(func() bool { return true })
+	m["bad-apple"] = &v
+	resm, ok = CastMapString(m)
+	assert.False(t, ok, "Not entirely valid map could not be casted")
+}
+
+func TestCastMapStringString(t *testing.T) {
+	res, err := CastPartialMapValue(vof(_testCastMap()), createStringType(), createStringType())
+	assert.Nil(t, err, "No casting error")
+	assert.Equal(t, map[string]string{"2": "3", "4": "5", "6.5": "1.5", "a": "b", "c": "1"}, res.Interface(), "Casted matches assumption")
+	resm, ok := CastMapStringString(res.Interface())
+	assert.True(t, ok, "Valid map could be casted")
+	assert.Equal(t, map[string]string{"2": "3", "4": "5", "6.5": "1.5", "a": "b", "c": "1"}, resm, "Result matches input")
+	resm, ok = CastMapStringString(_testCastMap())
+	assert.False(t, ok, "Not entirely valid map could not be casted")
+}
+
+func TestCastMapStringInt(t *testing.T) {
+	res, err := CastPartialMapValue(vof(_testCastMap()), createStringType(), createIntType())
+	assert.Nil(t, err, "No casting error")
+	assert.Equal(t, map[string]int64{"6.5": 1, "c": 1, "2": 3, "4": 5}, res.Interface(), "Casted matches assumption")
+	resm, ok := CastMapStringInt(res.Interface())
+	assert.True(t, ok, "Valid map could be casted")
+	assert.Equal(t, map[string]int64{"6.5": 1, "c": 1, "2": 3, "4": 5}, resm, "Result matches input")
+	resm, ok = CastMapStringInt(_testCastMap())
+	assert.False(t, ok, "Not entirely valid map could not be casted")
+}
+
+func TestCastMapStringFloat(t *testing.T) {
+	res, err := CastPartialMapValue(vof(_testCastMap()), createStringType(), createFloatType())
+	assert.Nil(t, err, "No casting error")
+	assert.Equal(t, map[string]float64{"4": 5, "6.5": 1.5, "c": 1, "2": 3}, res.Interface(), "Casted matches assumption")
+	resm, ok := CastMapStringFloat(res.Interface())
+	assert.True(t, ok, "Valid map could be casted")
+	assert.Equal(t, map[string]float64{"4": 5, "6.5": 1.5, "c": 1, "2": 3}, resm, "Result matches input")
+	resm, ok = CastMapStringFloat(_testCastMap())
+	assert.False(t, ok, "Not entirely valid map could not be casted")
+}
